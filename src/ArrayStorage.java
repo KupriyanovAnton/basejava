913ccaeafdf99967bb09c8de19ad9416813407cb
Anton <kuprianov.anton@yandex.ru>
2020-08-26 15:10:09 +0400

    Реализация методов ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,66 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
 
     void clear() {
+        size = 0;
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        if (size < 10000) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = ArrayStorage.getIndex(storage, uuid, size);
+        if (index != -1) {
+            return storage[index];
+        } else {
+            return null;
+        }
+
     }
 
     void delete(String uuid) {
+        int index = ArrayStorage.getIndex(storage, uuid, size);
+        if (index == -1) return;
+        for (int i = index; i < size - 1; i++) {
+            storage[i] = storage[i + 1];
+        }
+        storage[size - 1] = null;
+        size--;
+    }
+
+    private static int getIndex(Resume[] storage, String uuid, int size) {
+        Resume tmp;
+        for (int i = 0; i < size; i++) {
+            tmp = storage[i];
+            if (tmp.toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        if (size != 0) {
+            return Arrays.copyOf(storage, size);
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
